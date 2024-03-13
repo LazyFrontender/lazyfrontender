7e749aaaa3425a1bacb58c66366fb25b5a6f234b
Javohir Hasanov <javohirhasanov3303@gmail.com>
2024-03-14 00:27:48 +0500

    add background animation, text gradient and aos animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
+import React, { useEffect } from 'react';
 import './App.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+
 export const App = () => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return (
     <div>
-      <h1 className="gradient-text">Soon</h1>{' '}
+      <h1
+        className="gradient-text zoom-in"
+        data-aos="flip-down"
+        data-aos-duration="2000"
+      >
+        Soon
+      </h1>
+      <div className="lines">
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+      </div>
     </div>
   );
 };
