3351b35380938054b15f17e29ae539cff89aeec6
Javohir Hasanov <javohirhasanov3303@gmail.com>
2024-03-14 21:18:32 +0500

    add react helmet

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,18 @@ import './App.css';
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // You can also use <link> for styles
 
+import { Helmet } from 'react-helmet';
+
 export const App = () => {
   useEffect(() => {
     AOS.init();
   }, []);
   return (
     <div>
+      <Helmet>
+        <title>Javohirs blog</title>
+      </Helmet>
+      
       <h1
         className="gradient-text zoom-in"
         data-aos="flip-down"
